123c9d6f68a93a7a32266f57f19daa1ab7c363cf
szymondlugolecki <szymon.dlugolecki77@gmail.com>
2023-02-13 22:15:02 +0100

    init 😮‍💨

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import '../app.css';
+	import { toasts, ToastContainer, FlatToast, BootstrapToast } from 'svelte-toasts';
+
+	const showToast = () => {
+		const toast = toasts.add({
+			title: 'Message title',
+			description: 'Message body',
+			duration: 10000, // 0 or negative to avoid auto-remove
+			placement: 'bottom-right',
+			type: 'info',
+			theme: 'dark',
+			placement: 'bottom-right',
+			type: 'success',
+			theme,
+			onClick: () => {},
+			onRemove: () => {}
+			// component: BootstrapToast, // allows to override toast component/template per toast
+		});
+
+		// toast.remove()
+	};
+</script>
+
+<slot />
