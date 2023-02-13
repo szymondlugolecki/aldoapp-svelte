313a05aa2a61bfb4cbf9a737a1f4d14bfd4ec778
szymondlugolecki <szymon.dlugolecki77@gmail.com>
2023-02-13 22:09:38 +0100

    init

---
+++ src/lib/toasts/index.ts
@@ -0,0 +1,4 @@
+import { toasts, ToastContainer, FlatToast, BootstrapToast } from 'svelte-toasts';
+import type { ToastProps } from 'svelte-toasts/types/common';
+
+export const VerificationToast: Partial<ToastProps> = {};
