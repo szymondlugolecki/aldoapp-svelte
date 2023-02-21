b970a9b8458f416fd6648fcd196548718050efb3
szymondlugolecki <szymon.dlugolecki77@gmail.com>
2023-02-21 23:07:51 +0100

    half of the work with navbar done

--- src/components/Layout/Navbar.svelte
+++ src/components/Layout/Navbar.svelte
@@ -2,17 +2,8 @@
 	import { page } from '$app/stores';
 	import { roleColors, roleNames, salesmenMenu, shopMenu } from '$lib/client/constants';
 	import { capitalize } from '$lib/client/functions';
-	import {
-		NavBrand,
-		NavLi,
-		NavUl,
-		Avatar,
-		Badge,
-		Button,
-		Chevron,
-		MegaMenu
-	} from 'flowbite-svelte';
-	import { LogOut, History, Lock } from 'lucide-svelte';
+	import { Avatar, Badge } from 'flowbite-svelte';
+	import { LogOut, User, Lock } from 'lucide-svelte';
 	import type { Role, SessionUser } from '../../types';
 
 	$: activeUrl = $page.url.pathname.toLowerCase();
@@ -103,41 +94,16 @@
 	<div
 		class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5"
 	>
-		<a href="https://flowbite.com" class="flex items-center">
-			<img
-				src="https://flowbite.com/docs/images/logo.svg"
-				class="h-6 mr-3 sm:h-9"
-				alt="Flowbite Logo"
-			/>
-			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
-				>Flowbite</span
+		<!-- Logo -->
+		<a href="/" class="flex items-center">
+			<span
+				class="ml-1 sm:ml-0 self-center whitespace-nowrap text-xl font-semibold dark:text-white mr-1 sm:mr-2"
+				>Twoje</span
 			>
+			<img src="/logo.png" class="h-9 sm:h-11" alt="Logo ALDO" />
 		</a>
-		<button
-			on:click={() => (menuMenuHidden = !menuMenuHidden)}
-			type="button"
-			class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-			aria-controls="mega-menu-full"
-			aria-expanded="false"
-		>
-			<span class="sr-only">Open main menu</span>
-			<svg
-				class="w-6 h-6"
-				aria-hidden="true"
-				fill="currentColor"
-				viewBox="0 0 20 20"
-				xmlns="http://www.w3.org/2000/svg"
-				><path
-					fill-rule="evenodd"
-					d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-					clip-rule="evenodd"
-				/></svg
-			>
-		</button>
-		<div
-			id="mega-menu-full"
-			class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-		>
+		<!-- Main Menu - Navbar -->
+		<div id="mega-menu-full" class="items-center justify-between hidden w-full md:flex md:w-auto">
 			<ul
 				class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
 			>
@@ -189,7 +155,87 @@
 				</li>
 			</ul>
 		</div>
+
+		<!-- Avatar & User Menu Button -->
+		<div class="flex justify-center items-center">
+			<button
+				on:click={() => (menuMenuHidden = !menuMenuHidden)}
+				type="button"
+				class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+				aria-controls="mega-menu-full"
+				aria-expanded="false"
+			>
+				<span class="sr-only">Otwórz główne menu</span>
+				<svg
+					class="w-6 h-6"
+					aria-hidden="true"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+						clip-rule="evenodd"
+					/></svg
+				>
+			</button>
+			{#if user}
+				<div class="flex">
+					<button class="rounded-full" on:click={() => (userMenuHidden = !userMenuHidden)}>
+						<span class="sr-only">Open user menu</span>
+						<Avatar id="avatar-menu" border />
+					</button>
+					{#if !userMenuHidden}
+						<div
+							class="max-w-[220px] absolute top-11 right-1 z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+							id="user-dropdown"
+						>
+							<div class="px-4 py-3">
+								<Badge large color={roleColors[user.role]}>{roleNames[user.role]}</Badge>
+								<span class="block text-sm text-gray-900 dark:text-white">{user.fullName}</span>
+								<span class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400"
+									>{user.email}</span
+								>
+							</div>
+							<ul class="py-2" aria-labelledby="user-menu-button">
+								{#if ['admin', 'moderator'].includes(user.role)}
+									<li>
+										<a
+											href="/admin"
+											class={`w-full text-left flex justify-start items-center px-4 py-2 text-sm text-red-500 hover:bg-gray-100 dark:hover:bg-gray-600`}
+											><Lock class="mr-2 text-red-500" /> Panel administracyjny</a
+										>
+									</li>
+								{/if}
+								<li>
+									<a
+										href={`/uzytkownik/${user.id}`}
+										class="flex justify-start items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+										><User class="mr-2" /> Mój profil</a
+									>
+								</li>
+
+								<li class="h-0 w-full px-3 my-2">
+									<div class="border-t border-gray-300 border-dashed" />
+								</li>
+
+								<li>
+									<button
+										on:click={() => {
+											// should submit a logout form here!
+										}}
+										class="w-full flex justify-start items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+										><LogOut class="mr-2" /> Wyloguj</button
+									>
+								</li>
+							</ul>
+						</div>
+					{/if}
+				</div>
+			{/if}
+		</div>
 	</div>
+	<!-- Kontakty Menu -->
 	{#if !menuMenuHidden}
 		<div
 			id="mega-menu-full-dropdown"
