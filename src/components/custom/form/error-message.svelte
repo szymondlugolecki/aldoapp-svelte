4057e10802b91e9ef86acc3f829d7f237127ea3f
szymondlugolecki <szymon.dlugolecki77@gmail.com>
2024-06-20 00:10:21 +0200

    changed folder names to lower case

---
+++ src/components/custom/form/error-message.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let errors: string[] | undefined;
+</script>
+
+{#if errors && errors.length}
+	<div class="py-1.5">
+		<p class="text-sm text-destructive">{errors[0]}</p>
+	</div>
+{/if}
---
+++ src/components/custom/form/message.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let message: string | undefined;
+</script>
+
+{#if message}
+	<div class="py-1.5">
+		<p class="text-sm text-green-500/70">{message}</p>
+	</div>
+{/if}
