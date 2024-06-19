b5424b209a1616512a5e2d575bad8683223fbcf1
szymondlugolecki <szymon.dlugolecki77@gmail.com>
2024-06-20 00:13:05 +0200

    chore: Remove unused form and table components

--- src/components/custom/Form/Message.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let message: string | undefined;
-</script>
-
-{#if message}
-	<div class="py-1.5">
-		<p class="text-sm text-green-500/70">{message}</p>
-	</div>
-{/if}
--- src/components/custom/Table/table-hyperlink.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	export let href: string;
-	export let text: string;
-</script>
-
-<a class="underline" {href}>{text}</a>
